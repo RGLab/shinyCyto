79f25a9d8c8f6aafe399cef4e85d6b2213088837
Greg Finak <greg.finak@gmail.com>
2015-09-17 10:55:14 -0700

    update

--- R/app.R
+++ R/app.R
@@ -3,174 +3,141 @@ library(flowWorkspace)
 library(openCyto)
 library(shinyjs)
 library(data.table)
+library(networkD3)
+library(ggcyto)
 library(DT)
+library(hash)
+
+H = hash()
+
+maketreelist <- function(df, root=df[1,1]) {
+  if(is.factor(root)) root<-as.character(root)
+  r <- list(name=root)
+  children = df[df[,1]==root,2]
+  if(is.factor(children)) children<-as.character(children)
+  if(length(children)>0) {
+    r$children <- lapply(children, maketreelist, df=df)
+  }
+  r
+}
 #Test if a directory contains a GatingSet
 #TODO support for GatingSetList
 .isGatingSet = function(x){
-  x = lapply(x,function(x)list.files(as.character(x)))
+  x = lapply(x,function(x)list.files(as.character(x),recursive=FALSE))
+  if(!all(lengths(x)>0)){
+    return(FALSE)
+  }
   unlist(lapply(x,function(x)all(grepl("*.pb",x)|grepl("*.dat",x)|grepl("*.nc",x)|grepl("*.rds",x))))
+}
+.isGatingSetList = function(x){
+  x = lapply(x,function(x)list.files(as.character(x),recursive=FALSE))
+  if(any(grepl("\\.rds",x))){
+    x = x[!grepl("\\.rds",x)]
+    all(unlist(lapply(x,function(x).isGatingSet(x))))  
+  }else{
+    FALSE
+  }
+  
 }
 datadirectory = "../inst/extdata"
 ui <-   navbarPage("OpenCyto",
-               tabPanel("Data",{
+               tabsetPanel(tabPanel("Data",{
                navlistPanel(
-                 tabPanel("Upload Files",
+                 tabPanel("Import",
                           tags$br(),
-                          div(fileInput("filechooser",h5("Upload FCS Files and/or XML workspaces"), multiple = TRUE),
-                          h6(DT::dataTableOutput(outputId = "chooser")), style = 'width:90%;'),
+                          div(div(textInput("path_import",label = "Pick a directory of FCS files and/or workspaces.",value = "../inst/extdata"),style="display:inline-block;"),div(actionButton(inputId = "refresh_import",label = "Scan"),style="display:inline-block;"),style="display:inline-block;"),
+                          div(h6(DT::dataTableOutput(outputId = "file_table",))),
                           actionButton("parse_chosen",label="Import Files"),
-                          hidden(div(div(actionButton("parseGroup",label = "Parse Workspace"),style="display:inline-block"),div(uiOutput("workspaceGroups"),style="display:inline-block"),div(textOutput("nsamples"),style="display:inline-block"),style='width:90%',id="parseUI")),
+                          hidden(div(div(actionButton("parseGroup",label = "Parse Workspace"),style="display:inline-block;"),div(uiOutput("workspaceGroups"),style="display:inline-block"),div(textOutput("nsamples"),style="display:inline-block"),style='width:90%',id="parseUI")),
                           div(verbatimTextOutput("message1"),style='width:90%')
                           ),
                  
-                 tabPanel("Existing Data",
+                 tabPanel("Load",
                    div(div(h5("Existing Data"),style="display:inline-block"),
-                   div(actionButton(inputId = "refresh",label = "Refresh List"),style="display:inline-block"),style="display:inline-block"),
+                   div(div(textInput("path",label = "path",value = "../inst/extdata"),style="display:inline-block"),actionButton(inputId = "refresh",label = "Scan"),style="display:inline-block"),style="display:inline-block"),
                    div(h6(DT::dataTableOutput(outputId = "existing_data")), style = 'width:90%;'),
                    actionButton(inputId = "load",label = "Load Data"),
                    div(verbatimTextOutput("message"),style='width:90%')
                    
-                 ),useShinyjs(),id="datanavlist"
+                 ),useShinyjs(),extendShinyjs(script = "www/actions.js")
+,id="datanavlist"
                )
                
              }),
              
              tabPanel("Compensate & Transform","Compensate & Transform"),
              tabPanel("Gating Template","Build a Gating Template"),
-             tabPanel("Tree","View Gating Tree"),
-             id = "tabs"
-             
+             tabPanel("Tree","View Gating Tree",
+                      div(div(diagonalNetworkOutput("tree",width="400px",height="300px"),
+                      style="display:inline-block;float:left;"),div(imageOutput("gateplot",width = "300px",height="300px"),style="margin-left:400px;"),style="width:100%;height:100%;")
+                      ),id = "tabset"
              )
+)
 server <- function(input, output,session){
   MAX_MB_UPLOAD = 1024 # one Gb limit.
   options(shiny.maxRequestSize=MAX_MB_UPLOAD*1024^2)
  
-  .getFilePaths = reactive({
-  s = input$chooser_rows_selected
-  if(length(s)){
-    df = input$filechooser
-    setDT(df)
-    df[name%in%s,datapath]
-  }
-  })
+#   .getFilePaths = reactive({
+#   s = input$chooser_rows_selected
+#   if(length(s)){
+#     df = input$filechooser
+#     setDT(df)
+#     df[name%in%s,datapath]
+#   }
+#   })
   
-  .getFileName = reactive({
-    s = input$chooser_rows_selected
-    if(length(s)){
-      df = input$filechooser
-      setDT(df)
-      df[name%in%s,name]
-    }
-  })
-  .getChosenFileTable = reactive({
-    s = input$chooser_rows_selected
-    if(length(s)){
-      df = input$filechooser
-      setDT(df)
-      df
-    }
-  })
-  readyForXMLParsing<<-FALSE
-  observe({
-    if(length(.getFileName())==0){
-      shinyjs::hide("workspaceGroups")
-      shinyjs::hide("parseGroup")
-      shinyjs::hide("nsamples")
-      shinyjs::hide("parseUI")
-    }else if(grepl("\\.xml",.getFileName())){
-      shinyjs::show("workspaceGroups")
-      shinyjs::show("parseGroup")
-      shinyjs::show("nsamples")
-      shinyjs::disable("parseGroup")
-      shinyjs::show("parseUI")
-    }
-  })
-  
-  observeEvent(input$chooser_rows_selected,{
-    output$message1 = renderPrint({
-      fn = .getFileName()
-      if (length(fn)) {
-        if (all(grepl("*\\.xml",fn))&length(fn)==1) {
-          readyForXMLParsing<<-TRUE
-          
-          cat("Ready to import xml file ",fn)
-        }else if(all(grepl("*\\.fcs",fn))){
-          readyForXMLParsing<<-FALSE
-          cat("Ready to convert FCS files to GatingSet.")
-        }
-      }else{
-        readyForXMLParsing<<-FALSE
-        cat("Please choose an xml file or some FCS files and click 'parse' to create a gating set.")
-      }
+  rv = reactiveValues()
+  observeEvent(rv$gs,{
+    output$tree = renderDiagonalNetwork({
+      tree = getPopStats(rv$gs[1],format="long",showHidden=TRUE)[,.(Parent,Population)]
+      diagonalNetwork(maketreelist(data.frame(tree),root="root"),fontSize=8,margin = c(100,100))
     })
   })
+  
+  observeEvent(input$tabset,{
+    js$fire()
+  })
+
+  
+  observeEvent(input$refresh_import,{
+    datadirectory=input$path_import
+    output$file_table = DT::renderDataTable(
+      data.table(data=list.files(
+        datadirectory, full.names = TRUE, recursive = FALSE
+      ))[(data %like% "fcs" | data %like% "xml")]
+      ,rownames = FALSE,selection = "multiple")
+    output$message1 = renderPrint(cat("Select an xml file, or a set of FCS files."))
+    shinyjs::disable("parse_chosen")
+  })
+  
+
   observeEvent(input$parse_chosen,{
-      if (readyForXMLParsing&length(.getFileName())==1) {
-        output$message1 = renderPrint(cat("Copying data into place and parsing xml.\n"))
-        tbl = .getChosenFileTable()
-        tmp = tempdir()
-        copy_success = file.copy(from = tbl[,datapath],to = file.path(tmp,tbl[,name]), overwrite = TRUE)
-        if(all(copy_success)){
-          output$message1 = renderPrint({
-            cat("Successfully copied all files.\n")
-            cat("Parsing\n")
-            })
-          ws <<- openWorkspace(file.path(tmp,tbl[name%in%input$chooser_rows_selected,name]))
-          groups = getSampleGroups(ws)
-          samples = getSamples(ws)
-          setDT(groups)
-          setDT(samples)
-          samplegroups = merge(groups,samples,by="sampleID")
-          output$message1 = renderPrint({
-            if(class(ws)=="flowJoWorkspace"){
-              enable("parseGroup")
-              cat("Successfully read workspace! Ready to parse FCS files.")
-            }
-          })
-          groups = unique(groups[,.(groupName,groupID)])
-          groups = groups[,.(ID = .I),.(groupName,groupID)]
-          output$workspaceGroups = renderUI(selectInput("workspaceGroups",label = "Workspace Groups", choices = plyr::dlply(groups,.variables="groupName",.fun=function(x)x$ID)))
-          observeEvent(input$workspaceGroups,{
-            output$nsamples = renderText(paste0(groups[,.N,.(groupID)][groupID==input$workspaceGroups,N]," samples"))
-          })
-        }else{
-          output$message1 = renderPrint(cat("Failed to copy :",tbl[!copy_success,name],"\n"))
+      if (length(input$file_table_rows_selected)) {
+        tbl = input$file_table_rows_selected
+        if(grepl("\\.xml$",tbl)){
+          output$message1 = renderPrint(cat("parsing xml."))
+        }else if(all(grepl("\\.fcs$",tbl))){
+          output$message1 = renderPrint(cat("parsing fcs."))
         }
-      }else if (!readyForXMLParsing&length(.getFileName())){
-        readyForXMLParsing <<- FALSE
-        output$message1 = renderPrint(cat("Copying data into place and creating a gating set from selected fcs files.\n"))
-        tbl = .getChosenFileTable()
-        tmp = tempdir()
-        copy_success = file.copy(from = tbl[,datapath],to = file.path(tmp,tbl[,name]), overwrite = TRUE)
-        if(all(copy_success)){
-          output$message1 = renderPrint({
-            cat("Successfully copied all files.\n")
-            cat("Creating gating set\n")
-          })
-          fs = try(read.ncdfFlowSet(file.path(tmp,tbl[,name])))
-          if(!inherits(fs,"try-error")){
-            output$message1 = renderPrint(cat("Created ncdfFlowSet."))
-            gs <<- try(GatingSet(fs))
-            if(!inherits(gs,"try-error")){
-              output$message1 = renderPrint({cat("GatingSet successfully created!")})
-            }else{
-              output$message1 = renderPrint(cat("Failed to create a GatingSet"))
-            }
-          }else{
-            output$message1 = renderPrint(cat("Failed to create a FlowSet"))
-          }
-        }else{
-          output$message1 = renderPrint(cat("Failed to copy :",tbl[!copy_success,name],"\n"))
-        }
-      }else{
-        readyForXMLParsing <<- FALSE
-        output$message1 = renderPrint(cat("Please choose an xml file or some FCS files and click 'parse' to create a gating set.\n"))
       }
-    # })
+  })
+
+  observeEvent(input$file_table_rows_selected,{
+    if (length(input$file_table_rows_selected)) {
+      tbl = input$file_table_rows_selected
+      if(sum(grepl("\\.xml$",tbl))==1&length(input$file_table_rows_selected)==1){
+        shinyjs::enable("parse_chosen")
+      }else if(all(grepl("\\.fcs$",tbl))){
+        shinyjs::enable("parse_chosen")
+      }else {
+        output$message1 = renderPrint(cat("Select an xml file, or a set of FCS files."))
+        shinyjs::disable("parse_chosen")
+      }
+    }
   })
   
   observeEvent(input$parseGroup,{
-    gs <<-try(parseWorkspace(ws,name = as.numeric(input$workspaceGroups),path=ws@path))
+    rv$gs <<-try(parseWorkspace(ws,name = as.numeric(input$workspaceGroups),path=ws@path))
     if(inherits(gs,"try-error")){
       output$message1 = renderText(paste(geterrmessage(),"\nMaybe you need to upload FCS files as well?"))
     }else if(class(gs)=="GatingSet"){
@@ -182,12 +149,12 @@ server <- function(input, output,session){
   
   # Refresh / filter existing data -----------------------------------------
   observeEvent(input$refresh,{
-    output$existing_data = DT::renderDataTable(setDT(subset(
-      data.frame(data = list.files(
+    datadirectory=input$path
+    output$existing_data = DT::renderDataTable(
+      data.table(data=list.files(
         datadirectory, full.names = TRUE, recursive = FALSE
-      ))[,("data"),drop = FALSE],!(data %like% "fcs" |
-                                     data %like% "xml")))
-      [.isGatingSet(data)],rownames = FALSE,selection = "single")
+      ))[,.(isdir=file.info(data)$isdir),.(data)][!(data %like% "fcs" | data %like% "xml")][isdir==TRUE]
+      ,rownames = FALSE,selection = "single")
   })
   
   output$chooser = DT::renderDataTable(input$filechooser[,c("name","size")],rownames=FALSE, selection='multiple')
@@ -196,14 +163,46 @@ server <- function(input, output,session){
   observeEvent(input$load,{
     output$message = renderPrint(cat("Choose a dataset"))
     s = input$existing_data_rows_selected
-    if(length(s)){
-      gs <<- try(load_gs(path = s))
-      if(inherits(gs,"try-error")){
-        output$message = renderPrint(cat("Not a gating set"))
+    if(is.null(s)){
+      output$message = renderPrint(cat("Choose a some files above"))
+      return(NA)
+    }
+    if(length(s)&.isGatingSet(s)){
+      output$message = renderPrint(cat("Loading GatingSet"))
+      rv$gs <<- try(load_gs(path = s))
+      if(inherits(rv$gs,"try-error")){
+        output$message = renderPrint(cat("Error Loading ",s,":\n",geterrmessage()))
       }else{
         output$message = renderPrint(cat("Loaded ",s))
       }
+    }else if (length(s)&.isGatingSetList(s)){
+      output$message = renderPrint(cat("Loading GatingSetList"))
+      rv$gs <<- try(load_gslist(path = s))
+      if(inherits(rv$gs,"try-error")){
+        output$message = renderPrint(cat("Error Loading ",s,":\n",geterrmessage()))
+      }else{
+        output$message = renderPrint(cat("Loaded ",s))
+      }
+    }else{
+      output$message = renderPrint(cat("Not a stored GatingSet or GatingSetList"))
     }
+  })
+  plt = reactive({
+        h = digest::digest(input$selnode)
+        if(has.key(h,H)){
+          return(list(src = H[[h]], alt = input$selnode))
+        }else{
+          tf = tempfile(fileext = ".png")
+          png(file=tf,width = 6*75,height=6*75,units = "px",res=75)
+          print(plotGate(rv$gs,input$selnode))
+          dev.off()
+          H[[h]] = tf 
+          return(list(src = H[[h]], alt = input$selnode))
+        }
+    })
+  
+  observeEvent(input$selnode,{
+    output$gateplot = renderImage(plt(),deleteFile = FALSE)
   })
   }
 shinyApp(ui = ui, server = server)
